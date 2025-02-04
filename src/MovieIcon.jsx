f641053cf2000fd8a8d38227f775af99873269ff
Shereen Moussa <79228158+shereenm88@users.noreply.github.com>
2025-02-03 22:01:57 -0500

    Initial commit

---
+++ src/MovieIcon.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MovieIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30" fill="currentColor">
+    <path d="M18 4l2 2v12l-2 2H6l-2-2V6l2-2h12m0-2H6c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2z"/>
+    <path d="M12 8.5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5zm0 5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5zm0 5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5z"/>
+  </svg>
+);
+
+export default MovieIcon;
